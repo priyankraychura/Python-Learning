c0222cae1a14a0ea0df4ffbb3f89d3f39e501b3f
priyankraychura <priyankrkaychura@gmail.com>
2024-05-30 23:32:57 +0530

    shutilModule file added

---
+++ 87. ShutilModule.py
@@ -0,0 +1,8 @@
+import shutil
+import os
+
+# shutil.copy("emp.py", "emp2.py")
+# shutil.copytree("clutteredFolder", "PerfectFolder")
+# shutil.move("clutteredFolder/1.1 Assignment.pdf", "Assignment.pdf")
+# shutil.rmtree("PerfectFolder")
+os.remove("Assignment.pdf")
