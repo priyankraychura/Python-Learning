a6fb7a52f9c6615050fa2977c300413a91d7efb7
priyankraychura <priyankrkaychura@gmail.com>
2024-05-31 00:07:44 +0530

    Exercise 10 added

---
+++ 90. Exercise-10.py
@@ -0,0 +1,3 @@
+# Use the NewsAPI and the requests module to fetch the daily news related to different topics. 
+# Go to: https://newsapi.org/ and 
+# explore the various options to build you application
