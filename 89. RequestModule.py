a2ca64aadd789ae1dd74b12feba5939bed45d364
priyankraychura <priyankrkaychura@gmail.com>
2024-05-31 00:02:19 +0530

    Request Module added

---
+++ 89. RequestModule.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify())
+for heading in soup.find_all("h2"):
+    print(heading.text)
+
+# url = "https://jsonplaceholder.typicode.com/posts"
+
+# data = {
+#     "title": 'Priyank',
+#     "body": 'bhai',
+#     "userId": 12,
+#   }
+# headers =  {
+#     'Content-type': 'application/json; charset=UTF-8',
+#   }
+# response = requests.post(url, headers=headers, json=data)
+
+# print(response.text)
